6881852ee88a5cfb95388328e503d041459a0d22
Guillaume <guillaume.gobert@gmail.com>
2018-04-10 15:45:58 +0200

    first commit

---
+++ dev/src/main/java/sirh_saisie/dev/App.java
@@ -0,0 +1,13 @@
+package sirh_saisie.dev;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
